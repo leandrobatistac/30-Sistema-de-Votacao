cdfffd6e619417f4fff17f28ef295f16450e7049
Leandro <leandrobcrv@gmail.com>
2023-09-14 15:22:25 -0300

    Teste Requisito 04

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,40 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
+
+  /**
+   * Construtor.
+   */
+
+  public GerenciamentoVotacao() {
+    this.pessoasCandidatas = new ArrayList<PessoaCandidata>();
+    this.pessoasEleitoras = new ArrayList<PessoaEleitora>();
+    this.cpfsComputados = new ArrayList<String>();
+  }
+  
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    // 
+  }
+
+  @Override
+  public void mostrarResultado() {
+    // 
+  }
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numeroCandidato) {
+    //
+  }
+
+  @Override
+  public void votar(String cpfEleitor, int numeroCandidato) {
+    // 
+  }
 }
