fba98d96f4c4bc26794e669271a1f0cf48d28531
Leandro <leandrobcrv@gmail.com>
2023-09-14 14:39:06 -0300

    Requisito 02

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,42 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  Integer numero;
+  Integer votos;
+
+  /**
+  * Construtor de Pessoa Candidata.
+  *
+  * @param nome Nome da pessoa candidata.
+  * @param numero Número da pessoa candidata.
+  */
+  public PessoaCandidata(String nome, int numero) {
+    super(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  // Getters
+  public int getNumero() {
+    return numero;
+  }
+  
+  public int getVotos() {
+    return votos;
+  }
+
+  // Setters
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  // Métodos
+  public void receberVoto() {
+    this.votos++;
+  }
 
 }
