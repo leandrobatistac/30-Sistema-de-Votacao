a52d8323cee9f9ef98284a9c8336715194d83b6a
Leandro <leandrobcrv@gmail.com>
2023-09-14 14:49:08 -0300

    Requisito 03

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,26 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  /**
+  * Construtor de Pessoa Eleitora.
+  *
+  * @param cpf CPF da pessoa candidata.
+  */
+  public PessoaEleitora(String nome, String cpf) {
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+
+  // Getters
+  public String getCpf() {
+    return cpf;
+  }
+
+  // Setters
+  public void SetCpf(String cpf) {
+    this.cpf = cpf;
+  }
 
 }
