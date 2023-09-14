050124575285fbe985776a43a72dd4f0f3a0a308
Leandro <leandrobcrv@gmail.com>
2023-09-14 14:14:26 -0300

    Requisito 01

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,13 @@
 package com.betrybe.sistemadevotacao;
 
 public class Pessoa {
-  
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String setNome(String nome) {
+    return this.nome = nome;
+  }
 }
